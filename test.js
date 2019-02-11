bf96a897e00c559fb8d5981eedbbd034c52e04b2
JimmyDeJesus <jimmydejesus101@gmail.com>
2019-02-10 19:41:07 -0500

    added a if else statement in the test.js file to display a true statment

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
 /*JavaScript function being refrenced for the index.html page
      -creates a JavaScript pop-up before the page loads.*/
 alert("Hello, World/Mundo/Sekai!");
+
+/*002.021 This if,else statement shows if the statement is true in this case
+          if youLikeSpace is true display "Your gonna carry that weight..."*/
+var youLikeSpace = true;
+
+if (youLikeSpace) {
+
+    document.write("Your gonna carry that weight...");
+  } else{
+
+    document.write("see you space cowboy...");
+  }
