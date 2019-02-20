5ca35fc2ab21533d97c919dc35d979a7114a1ae1
JimmyDeJesus <jimmydejesus101@gmail.com>
2019-02-20 00:37:40 -0500

    added the Practical example useing loops assignment branch and adjusted test.js to reflect branch adjustments

--- test.js
+++ test.js
@@ -2,25 +2,11 @@
      -creates a JavaScript pop-up before the page loads.*/
 alert("Hello, World/Mundo/Sekai!");
 
-/*Assignment-008 ~ The begining of our for loop method */
+/*Assignment-009 ~ The begining of our
+                    for loop method that lets us cycle through elements in the page and do things to them */
 var links = document.getElementsByTagName("a");
 
-/*Assignment-009 ~ The begining of our for loop method showing break and continue*/
-for (i = 0; i < 10; i++) {
-/*Assignment-009 ~ we are saying if i is equal to 5 or 3 "skip" those numbers in the loop*/
-    if (i === 5 || i === 3) {
-/*Assignment-009 ~ continue makes it so you just skip this blocks iteration of code*/
-      continue;
+for(i = 0; i < links.length; i++) {
 
-    }
-    console.log(i);
-
-    if (i === 7) {
-/*Assignment-009 ~ break ends the function and any progression of code from
-                      here on aka kills the code*/
-      break;
-
-    }
-
+      links[i].className = "link-" + i;
 }
-  console.log("I have broken out of the loop");
