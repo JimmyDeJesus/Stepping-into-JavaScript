53cb81485458dc1ac5bf0b88c182b0874eef9047
JimmyDeJesus <jimmydejesus101@gmail.com>
2019-02-20 22:00:18 -0500

    updated test.js file and added the variable scope

--- test.js
+++ test.js
@@ -2,13 +2,21 @@
      -creates a JavaScript pop-up before the page loads.*/
 alert("Hello, World/Mundo/Sekai!");
 
+/*Assignment-011 ~ gloabaly assigning the var average */
+var average = 0;
 /*Assignment-010 ~ we start useing the getAverage function to specify what we are dividing*/
-function getAverage (a,b,c,d,e,f){
+function getAverage (a,b){
 
-      var average = (a + b + c + d + e + f) / 6;
+      var average = (a + b) / 2; //local variable
       console.log(average);
       return average;
 }
 /*Assignment-010 ~ this is a statement calling the average up above*/
-var myResult = getAverage(7,8,9,10,11,12);
-console.log("the average is " + myResult);
+myResult = getAverage(7,11); //global variable
+
+function logResult(){
+
+console.log("the average is " + myResult + " inside the function");
+}
+
+logResult();
